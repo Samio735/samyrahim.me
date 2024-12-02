7b8d01c30f8ae0c02df4ca2e3af9a82075dd44bd
Samy <s_rahim@estin.dz>
2024-12-02 01:19:35 +0100

    h

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Atom - A Tailus template';
-export const SITE_DESCRIPTION = 'Modern free Tailwind CSS, Astro landing page template for startups | built with Tailus Themer';
-export const SITE_URL = 'https://atom.tailus.io';
-export const SITE_IMAGE = '/og.png';
+export const SITE_TITLE = 'Samy Rahim';
+export const SITE_DESCRIPTION = 'Samy Rahim, Full StackGenAI Solutions Engineer';
+export const SITE_URL = 'https://www.samyrahim.me/';
+export const SITE_IMAGE = 'og.png';
