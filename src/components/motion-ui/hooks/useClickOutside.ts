292ee068e68c2a078f38ce639b46ebdf0e294b09
Samy <s_rahim@estin.dz>
2024-12-12 15:05:57 +0100

    fix vercel bug ?

---
+++ src/components/motion-ui/hooks/useClickOutside.ts
@@ -0,0 +1,27 @@
+import { type RefObject, useEffect } from "react";
+
+function useClickOutside<T extends HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: MouseEvent | TouchEvent) => void
+): void {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent): void => {
+      const target = event.target as Node;
+      if (!ref.current || ref.current.contains(target)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [ref, handler]);
+}
+
+export default useClickOutside;
