b4041a34df4bb8dfbb8f5d38fabffe8800e94274
Samy <s_rahim@estin.dz>
2024-12-12 11:11:18 +0100

    outbound chat

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 import themer from "@tailus/themer";
 
 export default {
-    content: [
-        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-        "./node_modules/@tailus/themer-**/dist/**/*.{js,ts}"
-    ],
-    plugins: [
-        themer({
-            palette: {
-                extend: "oz",
-            },
-            radius: "smoothest",
-            background: "light",
-            border: "light",
-            padding: "large"
-        })
-    ],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@tailus/themer-**/dist/**/*.{js,ts}",
+  ],
+  plugins: [
+    themer({
+      palette: {
+        extend: "oz",
+      },
+      radius: "smoothest",
+      background: "light",
+      border: "light",
+      padding: "large",
+    }),
+    require("tailwind-scrollbar"),
+  ],
 } satisfies Config;
