068809199ada9840984ff29f6b37ed16b1900a52
Samy <s_rahim@estin.dz>
2024-12-14 01:17:05 +0100

    never trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  trailingSlash: "always", // or 'never' to be consistent
+  trailingSlash: "never", // or 'never' to be consistent
   build: {
     format: "directory", // This helps with clean URLs
   },
